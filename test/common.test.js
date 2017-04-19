1b795e26fb577b33c5a35b073b85d7458d2e775d
Paul Frazee <pfrazee@gmail.com>
2017-04-19 12:18:36 -0500

    Readd dat-node test

--- test/common.test.js
+++ test/common.test.js
@@ -1,12 +1,10 @@
 const test = require('ava')
 const fs = require('fs')
-// const Dat = require('dat-node')
+const Dat = require('dat-node')
 const tutil = require('./util')
 const pda = require('../index')
 
-// TODO
-// test('work with dat-node', async t => {
-//   var dir = tutil.tmpdir()
-//   var dat = await new Promise(resolve => Dat(tutil.drive, {dir}, (err, dat) => resolve(dat)))
-//   t.deepEqual(Object.keys(await pda.listFiles(dat, '/')), [])
-// })
+test('work with dat-node', async t => {
+  var dat = await new Promise(resolve => Dat(tutil.tmpdir(), (err, dat) => resolve(dat)))
+  t.deepEqual(Object.keys(await pda.readdir(dat, '/')), [])
+})
